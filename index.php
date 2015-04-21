97f00e3e4102253122aaaeb69e828269a7a50e38
Antoni Lunenko <antoni.lunenko@gmail.com>
2015-04-21 12:49:39 +0300

    002 template

--- index.php
+++ index.php
@@ -1,20 +1,23 @@
 <!DOCTYPE HTML>
 <html lang="en-US">
-    <?php require_once('blocks/services/head.html'); ?>
+<?php require_once('blocks/services/head.html'); ?>
     <body>
         <div class="b-page-wrap">
             <!-- Start Header -->
-            <?php require_once('blocks/general/header.html'); ?>
+<?php require_once('blocks/general/header.html'); ?>
             <!-- End Header -->
 
             <!-- Start Page Content -->
-            <?php $path_content = count($_GET) > 0 ? 'pages/' . $_GET['f'] : 'pages/main.html' ?>
-            <?php require_once($path_content); ?>
+<?php $path_content = count($_GET) > 0 ? 'pages/' . $_GET['f'] : 'pages/main.html' ?>
+<?php require_once($path_content); ?>
             <!-- End Page Content -->
 
             <!-- Start Footer-->
-            <?php require_once('blocks/general/footer.html'); ?>
+<?php require_once('blocks/general/footer.html'); ?>
             <!-- Emd Footer -->
         </div>
+		
+		<!-- scripts -->
+		<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
     </body>
 </html>
